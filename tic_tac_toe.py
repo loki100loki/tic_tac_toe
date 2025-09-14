3783c30207f3c12b3e0fefa3d8007249c0e8907f
loki100loki <luka.xunwelia@gmail.com>
2025-09-14 21:16:16 +0400

    added simple coments, added buttons to the windows

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -17,25 +17,43 @@ def new_game():
 
 
 Window = Tk()
+
+#customize the window
 def Gui():
   Iamage = PhotoImage(file="tct.png")
   Window.iconphoto(True, Iamage)
   Window.title("Tic Tac Toe")
-  Window.geometry("400x400")
+
 
 Gui()
-
+#randomly choose the first player
 players = ["X","O"]
-
 player = random.choice(players)
+
+#list of buttons
 buttuns = [["0","0","0"],
            ["0","0","0"],
            ["0","0","0"]]
 
+#adding buttons to the window
+butframe = Frame(Window)
+butframe.pack(side=BOTTOM)
 
-txt_lable = Label(text=f"now is {player} turn", font=("consoles",40))
+for row in range(3):
+    for col in range(3):
+        buttuns[row][col] = Button(butframe, text="", font=("consoles",40), width=5, height=3,
+                                  command=lambda r=row, c=col: next_turn(r, c))
+        buttuns[row][col].grid(row=row, column=col)
+
+
+
+
+#whos turn is know?
+txt_lable = Label(Window, text=f"{player} turn", font=("consoles",40))
 txt_lable.pack(side=TOP)
 
+
+#reset buttun 
 reset_buttun = Button(text="restart", font=("consoles",20), command=new_game)
 reset_buttun.pack(side=TOP)
 
